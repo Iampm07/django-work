dc4ead6e918e695e2d74d8580a1b64f5c050e866
Praveen Medla <pankajmendla@gmail.com>
2024-11-18 12:14:26 +0530

    created details,results html  in templates/polls updated views,models of polls app

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ app_name="polls"
 
 urlpatterns = [
     path("",views.index,name="index"),
-    path("detail/",views.detail,name="detail"),
+    path("detail/<int:pk>/",views.detail,name="detail"),
     path("<int:question_id>/results/",views.results,name="results"),
     path("<int:question_id>/vote/",views.vote,name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,14 +10,15 @@ def index(request):
         "latest_question_list":latest_question_list,
              }
     return render(request,"polls/index.html",context)
-def detail(request,question_id):
+def detail(request,pk):
     
-    question= get_object_or_404(Question,pk=question_id)
+    question= get_object_or_404(Question,pk=pk)
     return render(request,"polls/details.html",{"question":
             question })    
 def results(request,question_id):
-    response ="you are looking at the result of the question %s" 
-    return HttpResponse (response % question_id)
+    question= get_object_or_404(Question,pk=question_id) 
+    return render(request,"polls/results.html",{
+        "question":question})
 
 def vote(request,question_id):
     question=get_object_or_404(Question,pk=question_id)
@@ -34,4 +35,5 @@ def vote(request,question_id):
     else:
         selected_choice.votes = F("votes")+1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results",args=(question_id,)))
+        return HttpResponseRedirect(
+            reverse("polls:results",args=(question_id,)))
