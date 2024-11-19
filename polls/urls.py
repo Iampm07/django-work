8a70bac9f9c40cfb28c8656b852b041cf9c4a47a
Praveen Medla <pankajmendla@gmail.com>
2024-11-19 11:11:04 +0530

    completed server side and added html pages working is complete

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 app_name="polls"
 
@@ -9,3 +10,6 @@ urlpatterns = [
     path("<int:question_id>/results/",views.results,name="results"),
     path("<int:question_id>/vote/",views.vote,name="vote"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
